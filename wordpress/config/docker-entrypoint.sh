647fb4b1f1b7f40affe5bac2106ea97391dcd6c8
Pascal Martineau <pascal@websimple.com>
2023-04-13 09:58:36 -0400

    feat: configure xdebug

--- wordpress/config/docker-entrypoint.sh
+++ wordpress/config/docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# Load Apache environment variables
 . /etc/apache2/envvars
+
+# Remove existing PID file
+if [ -f /var/run/apache2/apache2.pid ]; then
+  rm -f /var/run/apache2/apache2.pid
+fi
 
 exec apache2 -DFOREGROUND "$@"
