dc6b05de3bafb56c2720615e596d19752a3b34b9
Pascal Martineau <pascal@lewebsimple.ca>
2023-05-01 09:25:15 -0400

    feat: set apache2 uid / gid at runtime

--- src/wordpress/config/docker-entrypoint.sh
+++ src/wordpress/config/docker-entrypoint.sh
@@ -4,6 +4,13 @@ set -e
 # Load Apache environment variables
 . /etc/apache2/envvars
 
+# Check UID / GID
+usermod --non-unique --uid ${APACHE2_UID} www-data
+groupmod --non-unique --gid ${APACHE2_GID} www-data
+if [[ `stat -c '%u' /var/www` != "${APACHE2_UID}" ]]; then
+  chown -R www-data:www-data /var/www /tmp
+fi
+
 # Remove existing PID file
 if [ -f /var/run/apache2/apache2.pid ]; then
   rm -f /var/run/apache2/apache2.pid
